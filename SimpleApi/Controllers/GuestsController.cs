19f40ab101a2fae232c0fb60dd94e830da76e92e
Duy Phan <dpha@netcompany.com>
2021-12-30 02:57:32 +0100

    Add GuestsController

---
+++ SimpleApi/Controllers/GuestsController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SimpleApi.Models;
+
+namespace SimpleApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GuestsController : ControllerBase
+    {
+        private static List<Guest> _guests = new()
+        {
+            new Guest { Id = 1, FirstName = "Duy", LastName = "Phan" },
+            new Guest { Id = 2, FirstName = "Gordon", LastName = "Kien" },
+            new Guest { Id = 3, FirstName = "Gavin", LastName = "Chi" },
+        };
+
+        // GET: api/<GuestsController>
+        [HttpGet]
+        public IEnumerable<Guest> Get()
+        {
+            return _guests;
+        }
+
+        // GET api/<GuestsController>/5
+        [HttpGet("{id}")]
+        public Guest Get(int id)
+        {
+            return _guests.FirstOrDefault(g => g.Id == id);
+        }
+
+        // POST api/<GuestsController>
+        [HttpPost]
+        public void Post([FromBody] Guest value)
+        {
+            _guests.Add(value);
+        }
+
+        // PUT api/<GuestsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Guest value)
+        {
+            _guests.Remove(_guests.FirstOrDefault(g => g.Id == id));
+            _guests.Add(value);
+        }
+
+        // DELETE api/<GuestsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _guests.Remove(_guests.FirstOrDefault(g => g.Id == id));
+        }
+    }
+}
---
+++ SimpleApi/Models/Guest.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleApi.Models
+{
+    public class Guest
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
