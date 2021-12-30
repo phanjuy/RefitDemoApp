1e2df9dd8c40fbf79bdae5707102794f4a33867a
Duy Phan <dpha@netcompany.com>
2021-12-30 14:18:30 +0100

    Another way to call API

---
+++ SimpleUI/DataAccess/IWeatherData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Refit;
+using SimpleUI.Models;
+
+namespace SimpleUI.DataAccess
+{
+    public interface IWeatherData
+    {
+        [Get("/WeatherForecast")]
+        Task<List<WeatherDto>> GetWeather();
+    }
+}
